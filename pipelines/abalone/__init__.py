5924db2ab85d6ebd950f5d03c647432d228d4475
GuillermoLB <guillelb99@gmail.com>
2024-12-04 22:39:44 +0100

    Clean up __init__.py files in pipelines and abalone packages

--- pipelines/abalone/__init__.py
+++ pipelines/abalone/__init__.py
@@ -1,6 +1,1 @@
 # __init__.py
-
-# This file marks the directory as a Python package and can be used to initialize package-level variables or import submodules.
-
-# Example of importing a submodule
-# from . import submodule_name
