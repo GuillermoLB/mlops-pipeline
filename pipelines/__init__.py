aa4ac6367dd24f253d54d63626e31296adc09288
GuillermoLB <guillelb99@gmail.com>
2024-12-04 22:35:18 +0100

    Add __init__.py files to initialize pipelines and abalone packages

---
+++ pipelines/__init__.py
@@ -0,0 +1,5 @@
+# This is the __init__.py file for the pipelines package.
+# You can use this file to initialize the package and import necessary modules.
+
+# Example import statement
+# from .module_name import ClassName, function_name
